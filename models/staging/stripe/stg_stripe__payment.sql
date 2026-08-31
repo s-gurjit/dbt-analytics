f116804b6e19889ddad92cfb75cfe6dcacfc1108
s_gurjit@yahoo.com <s_gurjit@yahoo.com>
2026-08-31 01:50:48 +0000

    added generic tests. clean run.

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -12,8 +12,8 @@ renamed as (
         id as payment_id,
         orderid as order_id,
         paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
+        status as payment_status,
+        amount / 100 as payment_amount,
         created as created_at
         -- _batched_at
 
